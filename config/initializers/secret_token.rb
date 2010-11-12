c18a8be40b16965e571d5180c8da030aee3c5268
B V Satyaram <bvsatyaram@chronus.com>
2010-11-12 15:05:35 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Epayment::Application.config.secret_token = '8b7cbd92f925121cfe4fc86df10e481a57ea0f58f104c5b2cac270ed39a3be016a504bbf5e14eedb386ac1334880af260420b9f3fc458d6f73d501d4a883dba2'
